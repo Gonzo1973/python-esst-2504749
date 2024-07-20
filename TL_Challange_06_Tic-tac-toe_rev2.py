956e71806fdd7124a07cf672bb4a6da866222273
Gonzo1973 <tlukl@web.de>
2024-07-20 16:39:25 +0200

    Revision 2 of Tic-tac-toe

---
+++ TL_Challange_06_Tic-tac-toe_rev2.py
@@ -0,0 +1,88 @@
+# Challange 06 - Tic Tac Toe - Revision 2
+"""
+Changes to Rev1:
+- playing in blank playing field for better visibility of player's choises
+- Function checkWinner:
+  not checking for the equal fields but for equal to player ('x' or 'o') 
+  Thus, with second parameter to use player ('x' or 'o') 
+- Introduction of check for "Unentschieden"
+"""
+
+import os
+
+# Function to show play field
+def show_playField(playField):
+  # Clearing the Screen
+  os.system('cls')
+
+  # Display playing field
+  print("\n ", playField[0]," | ", playField[1]," | ", playField[2])
+  print("-----------------")
+  print(" ", playField[3]," | ", playField[4]," | ", playField[5])
+  print("-----------------")
+  print(" ", playField[6]," | ", playField[7]," | ", playField[8])
+
+# Function to check on winning-condition
+  # pending..
+
+# Check if "player" is the winner
+def checkWinner(playField, player):
+  if playField[0] == player and playField[1] == player and playField[2] == player:
+    return True
+  elif playField[3] == player and playField[4] == player and playField[5] == player:
+    return True
+  elif playField[6] == player and playField[7] == player and playField[8] == player:
+    return True
+  elif playField[0] == player and playField[3] == player and playField[6] == player:
+    return True
+  elif playField[1] == player and playField[4] == player and playField[7] == player:
+    return True
+  elif playField[2] == player and playField[5] == player and playField[8] == player:
+    return True
+  elif playField[0] == player and playField[4] == player and playField[8] == player:
+    return True
+  elif playField[2] == player and playField[4] == player and playField[6] == player:
+    return True
+  else:
+    return False
+
+# Introduction of playing field to the players
+playField = ["1","2","3","4","5","6","7","8","9"]
+show_playField(playField)
+print("\nWelcome to our Tic Tac Toe Game! :-)")
+print("This is your playing field.\n")
+print("\nPlease use the numbers 1 to 9 to indicate your chosen field.")
+input("Press ENTER when your are ready.")
+
+# Clearing the playing field
+playField = [" "," "," "," "," "," "," "," "," "]
+show_playField(playField)
+
+winner = False
+
+# main
+while True:
+  # player x's turn
+  player = "x"
+  player_input = int(input("\nPlayer x: make your choise (empty field 1 to 9): "))
+  player_input_index = player_input - 1
+  playField [player_input_index] = "x"
+  winner = checkWinner(playField, player)
+  show_playField(playField)
+  if winner:
+    break 
+  
+
+  # player o's turn
+  player = "o"
+  player_input = int(input("\nPlayer o: make your choise (empty field 1 to 9): "))
+  player_input_index = player_input - 1
+  playField [player_input_index] = "o"
+  winner = checkWinner(playField, player)
+  show_playField(playField)
+  if winner:
+    break 
+  
+print("\nThe Winner is player ",player," !")
+
+
